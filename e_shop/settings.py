67c65e533062f5cf4da48a15dfaf4f0ffcd4c2cb
Kostiantyn Lytvynov <lytvynov.kostiantyn@gmail.com>
2023-02-10 12:30:25 +0200

    updated security

--- e_shop/settings.py
+++ e_shop/settings.py
@@ -27,12 +27,18 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv("DJANGO_SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = [
     '127.0.0.1',
     'LytvynovKostiantyn.pythonanywhere.com'
 ]
+
+SECURE_PROXY_SSL_HEADER = True
+# SECURE_SSL_REDIRECT = True
+
+SESSION_COOKIE_SECURE = True
+SCRF_COOKIE_SECURE = True
 
 AUTH_USER_MODEL = 'shop.User'
 
