6e81cd9fab099f2c627bd70bdb5656cecca5312b
Kostiantyn Lytvynov <lytvynov.kostiantyn@gmail.com>
2023-03-02 16:43:58 +0200

    name update

--- e_shop/urls.py
+++ e_shop/urls.py
@@ -1,4 +1,4 @@
-"""e_shop URL Configuration
+"""e_shop_django URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/4.0/topics/http/urls/
